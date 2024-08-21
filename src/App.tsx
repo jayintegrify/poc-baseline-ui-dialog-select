cad7a655264e351c95b07a4f2059ba3e89927f18
Jay Amin <jay.amin@integrify.com>
2024-08-21 01:38:22 -0500

    update to latest baseline-ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,15 @@
 import {
-  I18nProvider,
-  Modal as ModalRaw,
-  ModalProps,
-  ModalTrigger as ModalTriggerRaw,
-  ThemeProvider,
-  ModalContent,
+  ActionButton,
   Dialog,
   DialogTitle,
-  ActionButton,
+  I18nProvider,
+  Modal,
+  ModalContent,
+  ModalTrigger,
   Select,
+  ThemeProvider,
 } from "@baseline-ui/core";
-import { ReactNode } from "react";
 import "./App.css";
-
-const Modal = ModalRaw as React.FC<ModalProps & { children?: ReactNode }>;
-const ModalTrigger = ModalTriggerRaw as React.FC<{ children?: ReactNode }>;
 
 function App() {
   const items = [
@@ -46,7 +41,7 @@ function App() {
                 Open select, click on one of the options that span outside of
                 the dialog. It shouldn't close, but it will.
               </p>
-              <div style={{ width: "50%", margin: 'auto' }}>
+              <div style={{ width: "50%", margin: "auto" }}>
                 <Select items={items} placeholder="Select me" />
               </div>
               <p>Some space to make dialog a bit bigger.</p>
